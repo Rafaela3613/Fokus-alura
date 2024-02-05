7f23c99fd39949e5ad057afe5a5e86b802996313
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2024-02-05 09:21:22 -0300

    inserindo sons no temporizador

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const botoes = document.querySelectorAll('.app__card-button')
 const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 
 let tempoDecorridoEmSegundos = 5
 let intervaloId = null
@@ -63,27 +66,31 @@ function alterarContexto(contexto) {
             break;
     }
 }
-const contagemRegressiva = () =>{
+
+const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0){
-        zerar()
+        audioTempoFinalizado.play()
         alert('Tempo finalizado!')
+        zerar()
         return
     }
     tempoDecorridoEmSegundos -= 1
-    console.log('temporizador:' + tempoDecorridoEmSegundos)
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos)
 }
 
 startPauseBt.addEventListener('click', iniciarOuPausar)
 
-function iniciarOuPausar () {
+function iniciarOuPausar() {
     if(intervaloId){
+        audioPausa.play()
         zerar()
         return
     }
+    audioPlay.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
 function zerar() {
-    clearInterval(intervaloId)
+    clearInterval(intervaloId) 
     intervaloId = null
 }
